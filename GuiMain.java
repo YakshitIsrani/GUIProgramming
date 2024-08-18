d388c8e2794c19026a7356742e2aab0f709d229e
YakshitIsrani <yakshitisrani@gmail.com>
2024-08-18 20:18:21 +0530

    GuiMain.java

---
+++ GuiMain.java
@@ -0,0 +1,9 @@
+package GuiProgramming;
+
+public class GuiMain {
+    public static void main(String[] args){
+//        AdditionFrame frame=new AdditionFrame();
+
+        UnitConverterFrame frame=new UnitConverterFrame();
+    }
+}
